e13318324ba44fc335fecebdedd2580bfa22f429
Zach Shipko <zachshipko@gmail.com>
2020-03-15 23:03:59 -0700

    Get linking working (kinda)

--- build.rs
+++ build.rs
@@ -6,8 +6,21 @@ fn main() {
     .output()
     .unwrap()
     .stdout;
+
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let prefix = std::path::PathBuf::from(String::from_utf8(output).unwrap().trim());
-    println!("cargo:rustc-link-search={}", prefix.join("lib").display());
+
+    let lto_full = std::fs::read_link(prefix.join("lib").join("libLTO.so")).unwrap();
+
+    std::fs::copy(prefix.join("lib").join(&lto_full), &out_dir.join(&lto_full)).unwrap();
+
+    std::fs::copy(
+        prefix.join("lib").join("libLLVM.so"),
+        &out_dir.join("libLLVM.so"),
+    )
+    .unwrap();
+
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
     println!("cargo:rustc-link-lib=LLVM");
     println!("cargo:rustc-link-lib=LTO");
 }
